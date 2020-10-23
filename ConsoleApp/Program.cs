c199a512d6f5fb03839f62c3249872132d8b6d9a
carlCarlson6 <bullet.carlos@gmail.com>
2020-10-24 00:13:01 +0200

    asd

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -9,26 +9,17 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            List<Filter> filters = new List<Filter>()
+            List<Filter> productoFilters = new List<Filter>()
             {
                 new Filter("Name", OperatorFilter.Equal, new ValueFilter("Juan")),
                 new Filter("Price", OperatorFilter.GreaterThan, new ValueFilter("32.5")),
             };
 
-            SearchCriteria criteria = new SearchCriteria("Products", filters);
+            SearchCriteria productCriteria = new SearchCriteria("Products", productoFilters);
             Console.WriteLine(criteria.ToString());
 
             Console.WriteLine("--------------------------------");
 
-            List<Filter> filters1 = new List<Filter>()
-            {
-                new Filter("CustomerName", OperatorFilter.Equal, new ValueFilter("Alfreds Futterkiste")),
-                new Filter("Country", OperatorFilter.Equal, new ValueFilter("Germany")),
-                new Filter("Address", OperatorFilter.Equal, new ValueFilter("Obere Str. 57"))
-            };
-
-            SearchCriteria criteria2 = new SearchCriteria("Customers", filters1);
-            Console.WriteLine(criteria2.ToString());
         }
 
     }
